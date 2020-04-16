ece7f37d14ed29e36544ede985c3d3d450dd234a
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-16 16:25:14 -0400

    create-tables

--- migrations/20200416155222_create-recipes.js
+++ migrations/20200416155222_create-recipes.js
@@ -1,6 +1,48 @@
 
 exports.up = function(knex) {
-  
+  return (
+      knex.schema
+        .createTable('recipes', tbl => {
+            tbl.string('id').primary();
+            tbl.string('name', 255).notNullable().unique();
+        })
+        .createTable('steps', tbl => {
+            tbl.string('id', 255).primary();
+            tbl.string('step_order').notNullable();
+            // foreign key:
+            tbl
+                .string('recipe_id')
+                .notNullable()
+                .references('id')
+                .inTable('recipes')
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+            tbl.string('description').notNullable();
+        })
+        .createTable('quantity', tbl => {
+            tbl.string('id', 255).primary();
+            tbl
+                .string('ingredient_id')
+                .notNullable()
+                .references('id')
+                .inTable('ingredients')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
+            tbl.float('quantity').notNullable()
+            tbl.string('unit').notNullable()
+            tbl
+                .string('steps_id')
+                .notNullable()
+                .references('id')
+                .inTable('steps')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
+        })
+        .createTable('ingredients', tbl => {
+            tbl.string('id', 255).primary();
+            tbl.string('name').notNullable().unique();
+        })
+  )
 };
 
 exports.down = function(knex) {
